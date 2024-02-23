4507e95cc8a6069bb391795d6c2f389c0c8ff6bf
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-23 15:22:15 +0100

    add cicle and let for btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,16 @@
 console.log("hello world");
 
-let imageList = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"]
+let imageList = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"] // array di immagini
 
 console.log(imageList);
+
+let prev = document.querySelector(".prev")
+let next = document.querySelector(".next")
+
+
+for (let i = 0; i < imageList.length; i++) { // ciclo per lo scorrimento
+    const singleSlide = imageList[i];
+    
+    console.log(singleSlide);
+}
+
